76546a35b1b8008b96ce8b68a91f488a364c8e81
Jason G <jsnamorav2.0@gmail.com>
2024-07-10 15:32:05 +0800

    Create load-script.js

---
+++ jgvia-blog/load-script.js
@@ -0,0 +1,15 @@
+function include(file) {
+
+	let script = document.createElement('script');
+	script.src = file;
+	script.type = 'text/javascript';
+	script.defer = true;
+
+	document.getElementsByTagName('head').item(0).appendChild(script);
+
+}
+
+include(
+'https://cdn.jsdelivr.net/gh/jsn01000111/jsn01000111.github.io@main/jscript/x-timeago.js');
+include(
+'https://cdn.jsdelivr.net/gh/jsn01000111/jsn01000111.github.io@main/jgvia-blog/post-random.js');
